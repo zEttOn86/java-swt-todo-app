a4ad1852eb247631bd7d051b4652c667fe50dc65
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2023-12-14 22:17:55 +0900

    [WIP] Rename

--- todo/src/main/java/com/todo/app/TodoApp.java
+++ todo/src/main/java/com/todo/app/TodoApp.java
@@ -1,5 +1,6 @@
 package com.todo.app;
 
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -7,14 +8,15 @@ import org.eclipse.swt.widgets.Shell;
  * Hello world!
  *
  */
-public class App 
+public class TodoApp 
 {
+
     public static void main(String[] args) {
 
         Display display = new Display(); // アプリが OS に問い合わせる窓口
-        Shell shell = new Shell(display); // ウィンドウを表現するクラス
-        shell.setText("ウィンドウ名");
-        shell.open();
+        TodoApp todoApp = new TodoApp();
+
+        Shell shell = todoApp.open(display);
 
         while(!shell.isDisposed()) { // 終了指令がない間は無限ループ
             if(!display.readAndDispatch()) {
@@ -22,5 +24,21 @@ public class App
             }
         }
         display.dispose(); // リソースの解放
+    }
+
+    private Shell open(Display display){
+        Shell shell = new Shell(display);
+        shell.setLayout(new GridLayout(1, false)); // makeColumnsEqualWidth: 列をすべて同じ幅にするかどうか、trueの場合同じ
+        
+        // Set title
+        shell.setText("TODOリスト");
+
+        // Header composite
+
+        // Body composite
+
+
+        shell.open();
+        return shell;
     }
 }
